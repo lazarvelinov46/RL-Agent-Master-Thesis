335edd8ebe9b063b73e3d0b6a7ed97e3a34b0c8c
lazarvelinov46 <velinovlazar@gmail.com>
2025-02-14 15:19:39 +0100

    Completed initial Q table logic

--- The Incredible Machine/QTable.cpp
+++ The Incredible Machine/QTable.cpp
@@ -1,2 +1,62 @@
 #include "stdafx.h"
 #include "QTable.h"
+
+QTable::QTable()
+{
+	this->alpha = 0.1;
+	this->gamma = 0.9;
+}
+
+QTable::QTable(int numberOfStates, int numberOfActions)
+{
+	for (int i = 0;i < numberOfStates;i++) {
+		for (int j = 0;j < numberOfActions;j++) {
+			this->values[i][j] = 0.0;
+		}
+	}
+	this->alpha = 0.1;
+	this->gamma = 0.9;
+}
+
+double QTable::getQValue(int stateId, int actionId) const
+{
+	if (this->values.count(stateId) && this->values.at(stateId).count(actionId)) {
+		return this->values.at(stateId).at(actionId);
+	}
+	return 0.0;
+}
+
+void QTable::updateQValue(int stateId, int actionId, double reward, int nextStateId)
+{
+	double qValue = this->getQValue(stateId, actionId);
+	double maxNextQ = -DBL_MAX;
+	if (this->values.count(nextStateId)) {
+		for (const std::pair<int, double>& action : this->values.at(nextStateId)) {
+			maxNextQ = std::max(maxNextQ,action.second);
+		}
+	}
+	double newValue = qValue + this->alpha*(reward + gamma * maxNextQ - qValue);
+	this->values[stateId][actionId] = newValue;
+}
+
+//maybe not all actions but best possible
+int QTable::getAction(int stateId, double epsilon)
+{
+	std::srand(std::time(0));
+	if ((rand()/RAND_MAX) < epsilon) {
+		int action = rand() % this->values[stateId].size();
+		return action;
+	}
+	else {
+		int actionId = 0;
+		double maxValue = this->values[stateId][actionId];
+		for (int i = 1;i < this->values[stateId].size();i++) {
+			double value= this->values[stateId][i];
+			if (value > maxValue) {
+				maxValue = value;
+				actionId = i;
+			}
+		}
+		return actionId;
+	}
+}
--- The Incredible Machine/QTable.h
+++ The Incredible Machine/QTable.h
@@ -9,25 +9,14 @@ private:
 	double alpha;
 	double gamma;
 public:
-	QTable() {
-		this->alpha = 0.1;
-		this->gamma = 0.9;
-	}
-	QTable(int numberOfStates, int numberOfActions) {
-		for (int i = 0;i < numberOfStates;i++) {
-			for (int j = 0;j < numberOfActions;j++) {
-				this->values[i][j] = 0.0;
-			}
-		}
-		this->alpha = 0.1;
-		this->gamma = 0.9;
-	}
+	QTable();
+	QTable(int numberOfStates, int numberOfActions);
+	
 
-	double getQValue(int stateId, int actionId)const {
-		if (this->values.count(stateId) && this->values.at(stateId).count(actionId)) {
-			return this->values.at(stateId).at(actionId);
-		}
-		return 0.0;
-	}
+	double getQValue(int stateId, int actionId)const;
+
+	void updateQValue(int stateId, int actionId, double reward, int nextStateId);
+
+	int getAction(int stateId,double epsilon);
 };
 
